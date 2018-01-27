31485e37c7846516468a7781fa1ce16d1a1a439f
molenubar <molenubar@hotmail.com>
2018-01-27 13:39:28 -0800

    Added starting scene

---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour {
+
+	private static UIManager _instance;
+	public static UIManager singleton{ get{ return _instance; } }
+
+	void Awake()
+	{
+		if( _instance == null )//Singleton implemention
+			_instance = this;
+		else
+		{
+			Destroy( this.gameObject );
+			return;
+		}
+		DontDestroyOnLoad (this.gameObject);
+	}
+
+}
