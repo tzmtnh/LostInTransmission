e1186e3ba8043ba005f0370108b63fa4a7ec2c52
Matan's Laptop <matan@onehamsa.com>
2018-01-27 13:36:25 -0800

    We have basic enemy boxes

--- Assets/Scritps/Track.cs
+++ Assets/Scritps/Track.cs
@@ -6,14 +6,20 @@ using UnityEngine.Assertions;
 public class Track : MonoBehaviour {
 
 	public float speed = 1;
+	public float enemySpawnRate = 5;
+	public float laneWidth = 1;
+
+	float _traveledDistance = 0;
+	float _traveledDistanceDelta = 0;
 
 	Transform _lane1;
 	Transform _lane2;
 	float _laneLength;
 
-	float traveledDistance = 0;
+	List<Transform> _enemies = new List<Transform>();
+	float _lastEnemySpawnTime = 0;
 
-	private void Awake() {
+	void initLanes() {
 		_lane1 = transform.Find("Lanes");
 		Assert.IsNotNull(_lane1);
 		_lane1.name = "Lanes 1";
@@ -24,13 +30,9 @@ public class Track : MonoBehaviour {
 		_lane2.localPosition += new Vector3(0, 0, _laneLength);
 	}
 
-	private void Update() {
-		float dt = Time.deltaTime;
-		traveledDistance += speed * dt;
-
+	void updateLanes() {
 		Vector3 pos1 = _lane1.localPosition;
-		pos1.z = - (traveledDistance % _laneLength);
-		//Debug.Log(pos1.z + " -> " + traveledDistance);
+		pos1.z = -(_traveledDistance % _laneLength);
 		if (-pos1.z > _laneLength) {
 			//swap lanes
 			Transform temp = _lane1;
@@ -44,6 +46,45 @@ public class Track : MonoBehaviour {
 		Vector3 pos2 = pos1;
 		pos2.z += _laneLength;
 		_lane2.localPosition = pos2;
+	}
+
+	void updateEnemies() {
+		float t = Time.time;
+		float timeSinceLastEnemySpawn = t - _lastEnemySpawnTime;
+		if (timeSinceLastEnemySpawn >= enemySpawnRate) {
+			int lane = Random.Range(-1, 2);
+			_lastEnemySpawnTime = t;
+			GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+			Transform enemy = cube.transform;
+			enemy.SetParent(transform);
+			enemy.transform.localPosition = new Vector3(lane * laneWidth, 0, _laneLength / 2f);
+			_enemies.Add(enemy);
+		}
+
+		Vector3 offset = new Vector3(0, 0, _traveledDistanceDelta);
+		for (int i = _enemies.Count - 1; i >= 0; i--) {
+			Transform enemy = _enemies[i];
+			enemy.localPosition -= offset;
+
+			if (-enemy.localPosition.z > _laneLength / 2f) {
+				_enemies.RemoveAt(i);
+				Destroy(enemy.gameObject);
+			}
+		}
+	}
+
+	void Awake() {
+		initLanes();
+	}
+
+	void Update() {
+		float dt = Time.deltaTime;
+		float lastTraveledDistance = _traveledDistance;
+		_traveledDistance += speed * dt;
+		_traveledDistanceDelta = _traveledDistance - lastTraveledDistance;
+
+		updateLanes();
+		updateEnemies();
 	}
 
 }
