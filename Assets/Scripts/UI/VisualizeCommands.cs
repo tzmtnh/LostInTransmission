7b8eb25cc7cc623447b9425a571fa99fa2e6ba0d
molenubar <molenubar@hotmail.com>
2018-01-27 16:28:23 -0800

    Trying to capture player commands to spawn sonar visuals

---
+++ Assets/Scripts/UI/VisualizeCommands.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisualizeCommands : MonoBehaviour {
+
+	public GameObject sonar_prefab;
+	private static VisualizeCommands _instance;
+	public static VisualizeCommands singleton{ get{ return _instance; } }
+
+	void Awake()
+	{
+		if( _instance == null )//Singleton implemention
+			_instance = this;
+		else
+		{
+			Destroy( this.gameObject );
+			return;
+		}
+		DontDestroyOnLoad (this.gameObject);
+	}
+		
+	public void OnStart()
+	{
+		Player.OnCommand += OnSend;
+	}
+
+	public void OnDestroy()
+	{
+		Player.OnCommand -= OnSend;
+	}
+		
+	public void OnSend(Player.Command cmd, float delay)
+	{
+		
+	}
+
+	public void Update()
+	{
+		if (Player.OnCommand != null)
+		{
+			GameObject sonar = Instantiate<GameObject>( sonar_prefab );
+		}
+	}
+
+}
