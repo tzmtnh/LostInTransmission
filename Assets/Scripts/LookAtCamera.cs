d723da3f2786a43a9a585d365c82f8a371a36486
Matan's Laptop <matan@onehamsa.com>
2018-01-27 17:16:35 -0800

    Sprite astroid

---
+++ Assets/Scripts/LookAtCamera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCamera : MonoBehaviour {
+
+	Transform _camera;
+	Transform _transform;
+
+	void Start () {
+		_camera = Camera.main.transform;
+		_transform = transform;
+	}
+	
+	void Update () {
+		_transform.rotation = Quaternion.LookRotation(_camera.position - _transform.position, _camera.up);
+	}
+}
