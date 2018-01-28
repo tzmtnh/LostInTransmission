145b5fe93a4689f0d39b04b0223e2a3c1138dc21
Matan's Laptop <matan@onehamsa.com>
2018-01-27 20:30:51 -0800

    Pixelate

---
+++ Assets/Scripts/Pixelate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pixelate : MonoBehaviour {
+
+	public int numHeightPixels = 100;
+	RenderTexture _resampleRT;
+
+	void Awake() {
+		int w = Screen.width / Screen.height * numHeightPixels;
+		_resampleRT = new RenderTexture(w, numHeightPixels, 0);
+		_resampleRT.filterMode = FilterMode.Point;
+	}
+
+	void OnRenderImage(RenderTexture source, RenderTexture destination) {
+		Graphics.Blit(source, _resampleRT);
+		Graphics.Blit(_resampleRT, destination);
+	}
+}
