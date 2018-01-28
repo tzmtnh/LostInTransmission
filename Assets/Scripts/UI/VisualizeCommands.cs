e3ed31dee3f2dba3ab1d7fe3d975ecd8f7d50ef4
molenubar <molenubar@hotmail.com>
2018-01-27 17:36:08 -0800

    got the ship commands visualized but without delay implementation

--- Assets/Scripts/UI/VisualizeCommands.cs
+++ Assets/Scripts/UI/VisualizeCommands.cs
@@ -8,6 +8,9 @@ public class VisualizeCommands : MonoBehaviour {
 	private static VisualizeCommands _instance;
 	public static VisualizeCommands singleton{ get{ return _instance; } }
 
+	private static float CommandDelay = 0.0f;
+	private List<GameObject> spawned_prefab;
+
 	void Awake()
 	{
 		if( _instance == null )//Singleton implemention
@@ -33,10 +36,30 @@ public class VisualizeCommands : MonoBehaviour {
 	public void OnSend(Player.Command cmd, float delay)
 	{
         GameObject sonar = Instantiate<GameObject>(sonar_prefab);
+		sonar.transform.position = GameObject.Find ("Remote").transform.position;
+		sonar.transform.SetParent(GameObject.Find ("Remote").transform);
+		CommandDelay = delay;
+
+		if (spawned_prefab == null) {
+			spawned_prefab = new List<GameObject> ();
+			spawned_prefab.Add(sonar);
+		} else 
+		{
+			spawned_prefab.Add(sonar);
+		}
     }
 
 	void Update()
 	{
+		if (spawned_prefab != null) 
+		{
+			foreach (var x in spawned_prefab) {
+				var horizontal = x.transform.localPosition;
+				horizontal.x++; 
+				x.transform.localPosition = horizontal;
+			}
+
+		}
 
 	}
 
